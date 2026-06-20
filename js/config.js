79287dd83766e8209413fed3bc07687b57631ee0
Nikhil Nanjappa <94963727+NikhilNanjappa@users.noreply.github.com>
2026-06-20 09:38:35 +0100

    removing web checkout feature

--- js/config.js
+++
@@ -1,7 +0,0 @@
-window.EASYVAT_CONFIG = {
-  SUPABASE_URL: "https://gjroxeibghplwhsibnfx.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_dgz9zOO3B6hIQ-Mqti8SEA_a23StpL0",
-  // API_BASE_URL: "http://localhost:8000",
-  API_BASE_URL: "https://easy-vat-backend.fly.dev",
-  PRODUCT_REDIRECT_URL: "https://madnik.co.uk/product/easy-vat/index.html",
-};
